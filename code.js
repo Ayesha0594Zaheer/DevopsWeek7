98011f8f0256e8c8aa92bf3ee65ebe13c55d78be
Ayesha0594Zaheer <workingspaceofficial55@gmail.com>
2025-01-21 17:23:17 +0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,4 @@
+document.getElementById('clickButton').addEventListener('click', function() {
+    var message = document.getElementById('message');
+    message.style.display = 'block'; // Show the hidden message when button is clicked
+});
